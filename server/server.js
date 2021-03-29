e2ceafada274688b1baf95669bc0d653852fc37a
ninomiyadaisuke <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-03-29 13:30:13 +0900

    express server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
-const name = "nino"
-module.exports = {
-  name,
-}
+const express = require("express")
 
-console.log("=====> node server", process.version,module.exports);
+const app = express()
+
+app.get("/api/:message",(req, res) => {
+  res.status(200).send(`Here is your message: ${req.params.message}`)
+})
+
+app.listen(8000,() => console.log(`Server is running on port 8000`))
