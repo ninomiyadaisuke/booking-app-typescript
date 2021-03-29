36b2c6466bd7263acdfd1bc3c870b4c71ab13f23
ninomiyadaisuke <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-03-29 15:49:26 +0900

    Register page

--- client/src/auth/Register.tsx
+++ client/src/auth/Register.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Register:React.FC = () => {
+const Register: React.FC = () => {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = () => {
+    alert("send user info to backend");
+  };
+
+
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      show the form <button type="submit">Submit</button>{" "}
+    </form>
+  );
+
   return (
-    <div className="container-fluid h1 p-5 text-center">
-      Register page
-    </div>
-  )
-}
+    <form>
+      <div className="container-fluid bg-secondary p-5 text-center">
+        <h1>Register</h1>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">{registerForm()}</div>
+        </div>
+      </div>
+    </form>
+  );
+};
 
-export default Register
+export default Register;
