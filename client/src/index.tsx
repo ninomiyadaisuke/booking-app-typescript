886a9bd535d9ace2acf21ded61edfb8fab8d9fd2
ninomiyadaisuke <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-03-29 11:14:26 +0900

    Redux setup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+// {type: "LIG_IN_USER", payload: {name: "Ninomiya", role: "Seller"}}
+const authReducer = (state = {}, action:any) => {
+  switch (action.type) {
+    case "LOG_IN_USER":
+      return { ...state, ...action.payload };
+    case "LOGOUT":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  user: authReducer,
+});
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
