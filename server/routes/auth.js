e40ec4ec33950f2aa3bb23d926dd2aa1d3edd060
ninomiyadaisuke <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-03-30 09:57:01 +0900

    Register endpoint and json middleware

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { showMessage } from "../controlloers/auth"
+import { showMessage,register } from "../controlloers/auth"
 
 const router = express.Router()
 
 router.get("/:message", showMessage)
+router.post("/register", register)
 
 module.exports = router
