c03e58026b06a230b4010f94e648ef62e0772f17
ninomiyadaisuke <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-03-30 11:16:56 +0900

    Save user in database

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,8 @@
 import express from "express"
-import { showMessage,register } from "../controlloers/auth"
+import { register } from "../controlloers/auth"
 
 const router = express.Router()
 
-router.get("/:message", showMessage)
 router.post("/register", register)
 
 module.exports = router
