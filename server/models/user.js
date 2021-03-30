fa4516cb951443aa0b24193e108d89cf36f12fe9
ninomiyadaisuke <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-03-30 10:27:17 +0900

    Bcrypt hash password

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
 const { Schema } = mongoose;
 
 const userSchema = new Schema(
@@ -26,5 +28,21 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", function (next) {
+  let user = this;
+  if (user.isModified("password")) {
+    return bcrypt.hash(user.password, 12, function (err, hash) {
+      if (err) {
+        console.log("BCRYPT HASH ERR", err);
+        return next(err);
+      }
+      user.password = hash;
+      return next();
+    });
+  } else {
+    return next()
+  }
+});
 
 export default mongoose.model("User", userSchema);
