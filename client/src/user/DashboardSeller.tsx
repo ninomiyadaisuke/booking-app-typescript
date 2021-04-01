b39dd8876e2875ba1717b3b5a8e6b1163a3755e8
ninomiyadaisuke <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-04-01 10:03:43 +0900

    Conditional rendering for connected and not connected users

--- client/src/user/DashboardSeller.tsx
+++ client/src/user/DashboardSeller.tsx
@@ -2,8 +2,38 @@ import React from "react";
 import { DashboardNav } from "../components";
 import { ConnectNav } from "../components";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { userAuth } from "../types";
 
 const DashboardSeller: React.FC = () => {
+  const selector = useSelector((state: userAuth) => ({ ...state }));
+  const auth = selector.auth;
+
+  const connected = () => (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-10">
+          <h2>Connect with stripe</h2>
+        </div>
+        <div className="col-md-2">
+          <Link to="/hotels/new" className="btn btn-primary">
+            + Add New
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+
+  const notConnected = () => (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-10">
+          <h2>Connect with stripe</h2>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <>
       <div className="container-fluid bg-secondary p-5">
@@ -12,18 +42,8 @@ const DashboardSeller: React.FC = () => {
       <div className="conatainer p-4">
         <DashboardNav />
       </div>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-10">
-            <h2>Your Bookings</h2>
-          </div>
-          <div className="col-md-2">
-            <Link to="/hotels/new" className="btn btn-primary">
-              + Add New
-            </Link>
-          </div>
-        </div>
-      </div>
+      {auth && auth.user ? connected() : notConnected()}
+      {/* <pre>{JSON.stringify(auth,null,4)}</pre> */}
     </>
   );
 };
