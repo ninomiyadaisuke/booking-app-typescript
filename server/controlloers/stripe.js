b0e5f8d518528d3ff7e45069b7cb6b144a4a6ce5
ninomiyadaisuke <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-04-01 16:17:15 +0900

    Save stripe account id

--- server/controlloers/stripe.js
+++ server/controlloers/stripe.js
@@ -3,12 +3,15 @@ import Stripe from "stripe";
 
 const stripe = Stripe(process.env.STRIPE_SECRET);
 
-
 export const createConnectAccount = async (req, res) => {
   const user = await User.findById(req.user._id).exec();
   console.log("USER ===> ", user);
-  const account = await stripe.accounts.create({
-    type: "express",
-  });
-  console.log("ACCOUNT ===>", account);
+  if (!user.stripe_account_id) {
+    const account = await stripe.accounts.create({
+      type: "express",
+    });
+    console.log("ACCOUNT ===>", account);
+    user.stripe_account_id = account.id;
+    user.save();
+  }
 };
