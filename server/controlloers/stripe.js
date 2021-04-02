5f9353ef804573577c5b7493304b0af96d83d52a
ninomiyadaisuke <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-04-02 09:24:39 +0900

    Create login link

--- server/controlloers/stripe.js
+++ server/controlloers/stripe.js
@@ -1,5 +1,6 @@
 import User from "../models/user";
 import Stripe from "stripe";
+import queryString from "query-string";
 
 const stripe = Stripe(process.env.STRIPE_SECRET);
 
@@ -14,4 +15,17 @@ export const createConnectAccount = async (req, res) => {
     user.stripe_account_id = account.id;
     user.save();
   }
+  let accountLink = await stripe.accountLinks.create({
+    account: user.stripe_account_id,
+    refresh_url: process.env.STRIPE_REDIRECT_URL,
+    return_url: process.env.STRIPE_REDIRECT_URL,
+    type: "account_onboarding",
+  });
+  accountLink = Object.assign(accountLink,{
+    "stripe_user[email]": user.email || undefined,
+  });
+  let link = `${accountLink.url}?${queryString.stringify(accountLink)}`
+  console.log("LIGIN LINK",link);
+  res.send(link)
+  //  console.log("ACCOUNT LINK", accountLink);
 };
