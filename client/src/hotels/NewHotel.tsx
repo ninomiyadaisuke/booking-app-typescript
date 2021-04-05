5c7ea8a1e5160078ea4e1b3f617beb6ffb6aa719
ninomiyadaisuke <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-04-05 13:07:51 +0900

    New hotel form

--- client/src/hotels/NewHotel.tsx
+++ client/src/hotels/NewHotel.tsx
@@ -1,7 +1,94 @@
-import React from 'react'
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+// const AlgokiaPlaces = require("algolia-places-react");
 
-const NewHotel:React.FC = () => {
-  return <div className="container-fluid h1 p-5 text-center">Post a new hotel</div>;
-}
+const NewHotel: React.FC = () => {
+  const [values, setValues] = useState({
+    //state
+    title: "",
+    content: "",
+    location: "",
+    image: "",
+    price: "",
+    from: "",
+    to: "",
+    bed: "",
+  });
+  // destructuring variables from state
+  const { title, content, location, image, price, from, to, bed } = values;
 
-export default NewHotel
+  const handleSubmit = (e: React.FormEvent) => {
+    //
+  };
+
+  const handleImageChange = (e: React.ChangeEvent) => {};
+
+  const handleChange = (e: React.ChangeEvent) => {};
+
+  return (
+    <>
+      <div className="container-fluid bg-secondary p-5 text-center">
+        <h2>Add Hotel</h2>
+      </div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-10">
+            <br />
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label className="btn btn-outline-secondary btn-block m-2 text-left">
+                  Image
+                  <input
+                    type="file"
+                    name="image"
+                    onChange={handleImageChange}
+                    accept="image/*"
+                    hidden
+                  />
+                </label>
+                <input
+                  type="text"
+                  name="title"
+                  onChange={handleChange}
+                  placeholder="Title"
+                  className="form-control m-2"
+                  value={title}
+                />
+                <textarea
+                  name="content"
+                  onChange={handleImageChange}
+                  placeholder="Title"
+                  className="form-control m-2"
+                  value={content}
+                />
+
+                <input
+                  type="number"
+                  name="price"
+                  onChange={handleChange}
+                  placeholder="Price"
+                  value={price}
+                  className="form-control m-2"
+                />
+                <input
+                  type="number"
+                  name="bed"
+                  onChange={handleChange}
+                  placeholder="Number of Beds"
+                  value={bed}
+                  className="form-control m-2"
+                />
+              </div>
+              <button className="btn btn-outline-primary m-2">
+                Save
+              </button>
+            </form>
+          </div>
+          <div className="col-md-2">Image <pre>{JSON.stringify(values, null, 4)}</pre></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NewHotel;
